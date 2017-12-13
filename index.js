ecb519cdf9f143fd56a18886cc6eb269449cd8e4
Ivan Demidov <scrum@list.ru>
2017-12-13 19:00:49 +0300

    add nonce test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import posthtml from 'express-posthtml';
 
 const app = express();
-const plugins = [require('posthtml-cache')()];
+const plugins = [];
 const options = {};
 
 app.engine('html', posthtml);
@@ -11,8 +11,16 @@ app.set('view engine', 'html');
 app.set('views', './views');
 app.set('view options', { plugins: plugins, options: options });
 
+app.use('/static', express.static(__dirname + '/static'));
+
 app.get('/', (req, res) => {
 	res.render('index', function(err, html) {
+		res.set({
+			'Content-Security-Policy': 
+				'script-src \'strict-dynamic\' \'nonce-2726c7f26c\';' +
+				'style-src \'nonce-2726c7f26c\';' + 
+				'img-src \'nonce-2726c7f26c\''	
+		});
 		res.send(html);
 	});
 });
---
+++ static/index.js
@@ -0,0 +1,2 @@
+const text = window.variable === undefined ? 'variables not exits global' : variable;
+alert(text);
